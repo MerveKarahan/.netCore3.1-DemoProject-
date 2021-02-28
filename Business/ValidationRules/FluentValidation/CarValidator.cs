08176dc9ef0dbc740b949c6b7510d7b0893d1269
merve karahan <betulmervekarahan@gmail.com>
2021-02-28 21:04:18 +0300

    ReCapProject..

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Description).NotEmpty();
+            RuleFor(p => p.Description).NotNull();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.ModelYear).LessThan(DateTime.Now.Year);
+            RuleFor(p => p.ModelYear).NotEmpty();
+            RuleFor(p => p.BrandId).GreaterThan(0);
+            RuleFor(p => p.BrandId).NotEmpty();
+            RuleFor(p => p.ColorId).GreaterThan(0);
+            RuleFor(p => p.ColorId).NotEmpty();
+        }
+    }
+}
